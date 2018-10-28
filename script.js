d282827db6556c95819128ca1e5f17691bd7b976
Alexey Belyanin <fowku@MacBook-Pro-Alexey.local>
2018-10-28 23:52:43 +0300

    fix

---
+++ script.js
@@ -0,0 +1,7 @@
+function showHideEdit() {
+    const edit = document.getElementsByClassName('block-edit')[0];
+    if (edit.style.display === 'none')
+        edit.style.display = 'block';
+    else
+        edit.style.display = 'none';
+}

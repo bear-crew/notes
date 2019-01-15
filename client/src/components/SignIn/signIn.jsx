01994aeff022db7387536b9e57bfb6509d1cfc65
georgepsh <georgii39@gmail.com>
2019-01-16 02:08:54 +0300

    sign in form fixed

--- client/src/components/SignIn/signIn.jsx
+++ client/src/components/SignIn/signIn.jsx
@@ -2,15 +2,37 @@ import React, { Component } from 'react';
 import './signIn.css';
 
 class SignIn extends Component {
-    state = {    }
+    constructor (props) {
+        super(props);
+      
+        this.state = {
+            email: '',
+            password: '',
+        };
+      
+        this.logOn = this.logOn.bind(this);
+    }
+    
+    updateEmail(e) {
+        this.setState({email: e.target.value})
+    }
+
+    updatePassword(e) {
+        this.setState({password: e.target.value})
+    }
+
+    logOn() {
+        console.log(this.state.email)
+        console.log(this.state.password)
+    }
 
     render() {
         return[
             <div className="login-box">
                 <p>Save your notes in a cloud right now</p>
-                <input type="email" placeholder="your@email.com" className="login-input"></input>
-                <input type="password" placeholder="password" className="login-input"></input>
-                <button type="button" className="login-button">Login</button> 
+                <input value = {this.state.email} type="email" placeholder="your@email.com" className="login-input" onChange={(e) => {this.updateEmail(e); }}></input>
+                <input value = {this.state.password} type="password" placeholder="password" className="login-input" onChange={(e) => {this.updatePassword(e); }}></input>
+                <button type="button" className="login-button" onClick={this.logOn}>Login</button> 
             </div>
         ]
     }

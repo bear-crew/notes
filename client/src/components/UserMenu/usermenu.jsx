3cea0d8cee5924c56fcdfc00c882be68906a455d
fowku <chek970@gmail.com>
2019-01-17 00:47:43 +0300

    logout menu in work

---
+++ client/src/components/UserMenu/usermenu.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './usermenu.css';
+
+class UserMenu extends Component {
+    state = {  }
+    render() { 
+        return (
+            <div class="bubble">
+                
+            </div>
+        );
+    }
+}
+ 
+export default UserMenu;

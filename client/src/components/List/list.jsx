bd2ace8834ab7b5a1459fdad034b864bc4fb4ff2
fowku <chek970@gmail.com>
2019-01-17 17:45:22 +0300

    css fix

--- client/src/components/List/list.jsx
+++ client/src/components/List/list.jsx
@@ -22,7 +22,7 @@ class List extends Component {
                     </div>   
                     <p>Родилась 5 августа 1915 года в/на Украине в селе Первозвановка Полтавской губернии. В семь лет осталась полной сиротой. В семь лет осталась полной сиротой.</p>
                 </li>
-                {/* <li><div className="out"><h2>Заметка 6</h2></div></li>
+                <li><div className="out"><h2>Заметка 6</h2></div></li>
                 <li><div className="out"><h2>Заметка 7</h2></div></li>
                 <li><div className="out"><h2>Заметка 8</h2></div></li>
                 <li><div className="out"><h2>Заметка 9</h2></div></li>
@@ -32,7 +32,7 @@ class List extends Component {
                 <li><div className="out"><h2>Заметка 13</h2></div></li>
                 <li><div className="out"><h2>Заметка 14</h2></div></li>
                 <li><div className="out"><h2>Заметка 15</h2></div></li>
-                <li><div className="out"><h2>Заметка 16</h2></div></li> */}
+                <li><div className="out"><h2>Заметка 16</h2></div></li>
             </ul>
         );
     }

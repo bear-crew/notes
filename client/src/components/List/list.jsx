391da17eaea19b6b0feb1cdf2aba5354ba8aac7f
georgepsh <georgii39@gmail.com>
2019-01-19 00:33:01 +0300

    selectNote

--- client/src/components/List/list.jsx
+++ client/src/components/List/list.jsx
@@ -4,9 +4,14 @@ import {connect} from 'react-redux';
 
 class List extends Component {
     state = {  }
+
+    selectNote = (e) => {
+        console.log(e)
+    }
+
     render() {
         const list = this.props.noteList.map(
-            item => (<li key={item}><div className="out"><h2>{item.content.pole1}</h2></div><p>{item.content.pole2 ? item.content.pole2 : ""}</p></li>
+            item => (<li key={item._id}  onClick = {() => {this.selectNote(item._id)}}><div className="out"><h2>{item.content.pole1}</h2></div><p>{item.content.pole2 ? item.content.pole2 : ""}</p></li>
         ))
         return (
             <ul className="notes">
